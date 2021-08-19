72ca2a3dcd8bc48196d00226efa7c7d1ab9b0ab5
mltmercin92 <mltmercin92@gmail.com>
2021-08-19 18:23:30 +0300

    Kelime düzenleme,silme random kelime atama

--- hrApp/src/app/app.component.ts
+++ hrApp/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hrApp';
+  wordIndex = -1;
+  targetWordArray: number[] = [];
+  letter = '';
+  constructor() {}
+  ngOnInit(): void {
+    this.wordIndex = Math.floor(
+      Math.random() *
+        (JSON.parse(localStorage.getItem('wordList') || '{}') as []).length
+    );
+
+    var arrayLength = (
+      (
+        (JSON.parse(localStorage.getItem('wordList') || '{}') as [])[
+          this.wordIndex
+        ] as any
+      ).text as string
+    ).length;
+    for (let index = 0; index < arrayLength; index++) {
+      this.targetWordArray.push(0);
+    }
+    console.log(this.wordIndex);
+  }
+
+  Guess(letter: string): void {
+    var wordToFind = (
+      JSON.parse(localStorage.getItem('wordList') || '{}') as []
+    )[this.wordIndex];
+    var isLetterExist = ((wordToFind as any).text as string).includes(letter);
+    if (isLetterExist == false) {
+      console.log(' Yanlış tahmin.');
+    } else {
+      console.log('Doğru Tahmin.');
+    }
+  }
 }
--- hrApp/src/app/wordpool/wordpool.component.ts
+++ hrApp/src/app/wordpool/wordpool.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class WordpoolComponent implements OnInit {
   word = { text: '', meaning: '' };
   wordList: any[] = [];
-
+  isEditing: boolean = false;
+  indexToEdit: number = -1;
   constructor() {}
 
   ngOnInit(): void {
@@ -18,10 +19,30 @@ export class WordpoolComponent implements OnInit {
         : [];
   }
   Save(): void {
-    this.wordList.push(this.word);
+    if (this.word.text.length < 4) {
+      alert('Please  dont do  this. At least 4 letters accepted...');
+      return;
+    }
+    if (this.isEditing == false) {
+      this.wordList.push(this.word);
+      localStorage.setItem('wordList', JSON.stringify(this.wordList));
+      console.log(this.word);
+      console.log(this.wordList);
+      this.word = { text: '', meaning: '' };
+    } else {
+      this.wordList[this.indexToEdit] = this.word;
+      localStorage.setItem('wordList', JSON.stringify(this.wordList));
+      this.word = { text: '', meaning: '' };
+    }
+  }
+  deleteRecord(item: any): void {
+    var index = this.wordList.findIndex((x) => x.text == item.text);
+    this.wordList.splice(index, 1);
     localStorage.setItem('wordList', JSON.stringify(this.wordList));
-    console.log(this.word);
-    console.log(this.wordList);
-    this.word = { text: '', meaning: '' };
+  }
+  editRecord(item: any): void {
+    this.indexToEdit = this.wordList.findIndex((x) => x.text == item.text);
+    Object.assign(this.word, item);
+    this.isEditing = true;
   }
 }
